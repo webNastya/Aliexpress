22bebe5c42db69a3d1d26ae4509d123ef6bd73ab
xxiner <xxiner@mail.ru>
2020-12-30 14:16:04 +0300

    Add files via upload

--- Classes/Auth/controller/auth.js
+++ Classes/Auth/controller/auth.js
@@ -1,29 +1,28 @@
-const model = require('../model/auth')
-const path = require("path")
-const compileFile = require("pug").compileFile
+const model = require("../model/auth");
+const path = require("path");
+const compileFile = require("pug").compileFile;
 
 exports.get = (req, res) => {
-    // model.get(req, res, (data)=>{
-        res.send(compileFile(path.join(__dirname, '../view/auth.pug'))())
-    // })
-}
+  // model.get(req, res, (data)=>{
+  res.send(compileFile(path.join(__dirname, "../view/auth.pug"))());
+  // })
+};
 exports.post = (req, res) => {
-    // model.post(req, res, (data)=>{
-        res.send(compileFile(path.join(__dirname, "../view/auth.pug"))())
-    // })
-}
+  // model.post(req, res, (data)=>{
+  res.send(compileFile(path.join(__dirname, "../view/auth.pug"))());
+  // })
+};
 
 exports.register = (req, res) => {
-        model.register(req, res, (data)=>{
-                res.send(compileFile(path.join(__dirname, "../view/auth.pug"))(data))
-        })
-
-}
+  model.register(req, res, (data) => {
+    res.send(compileFile(path.join(__dirname, "../view/auth.pug"))(data));
+  });
+};
 
 exports.login = (req, res) => {
-        passport.authenticate("local", {
-                successRedirect: "/",
-                failureRedirect: "/auth",
-                failureFlash: true,
-        })(req, res, next);
-}
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/auth",
+    failureFlash: true,
+  })(req, res, next);
+};
--- Classes/Auth/model/auth.js
+++ Classes/Auth/model/auth.js
@@ -1,61 +1,69 @@
-const db = require('../../../db').get()
-const bcrypt = require("bcryptjs")
+const db = require("../../../db").get();
+const bcrypt = require("bcryptjs");
+var jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const { window } = new JSDOM();
+const { document } = new JSDOM("").window;
+global.document = document;
 
-exports.register = (req, res, callback)=> {
-    const loginPattern = new RegExp(/^[a-z0-9_-]{5,16}$/)
-    const passwordPattern = new RegExp(/^[a-z0-9_-]{5,16}$/)
+var $ = (jQuery = require("jquery")(window));
 
-    const { login, password, repassword } = req.body
-    let errors = []
-    if (!loginPattern.test(login)) {
-        errors.push("Ошибка ввода логина")
-    }
-    if (!passwordPattern.test(password) || password != repassword) {
-        errors.push("Ошибка ввода пароля")
-    }
+exports.register = (req, res, callback) => {
+  const loginPattern = new RegExp(/^[a-z0-9_-]{5,16}$/);
+  const passwordPattern = new RegExp(/^[a-z0-9_-]{5,16}$/);
 
+  const { regLogin, regPassword, repassword } = req.body;
+  let errors = [];
+  if (!loginPattern.test(regLogin)) {
+    errors.push("Ошибка ввода логина");
+  }
+  if (!passwordPattern.test(regPassword) || regPassword != repassword) {
+    errors.push("Ошибка ввода пароля");
+  }
+
+  let data = {
+    regLogin: regLogin,
+    regPassword: regPassword,
+    repassword: repassword,
+    errors: errors,
+    layout: "auth",
+  };
+  if (errors.length == 0) {
+    const collectionProfile = db.collection("profiles");
+    collectionProfile.findOne({ login: regLogin }).then((user) => {
+      if (user) {
+        //user already exists
+        errors.push("nickname already exists");
+        callback(data);
+      } else {
+        //user registration part
+        //user formation and registration
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(regPassword, salt, (err, hash) => {
+            if (err) throw err;
+            let passwordHash = hash;
+            collectionProfile.insertOne(
+              {
+                login: regLogin,
+                password: passwordHash,
+              },
+              function (err, profile) {
+                callback(data);
+              }
+            );
+          });
+        });
+      }
+    });
+  } else {
+    //error
     let data = {
-        login: login,
-        password: password,
-        repassword: repassword,
-        errors: errors,
-        layout: "auth"
-    }
-    if (errors.length == 0) {
-
-        const collectionProfile = db.collection("profiles")
-        collectionProfile.findOne({ login: login }).then((user) => {
-            if (user) {
-                //user already exists
-                errors.push("nickname already exists")
-                callback(data)
-            } else {
-                //user registration part
-                //user formation and registration
-                bcrypt.genSalt(10, (err, salt) => {
-                    bcrypt.hash(password, salt, (err, hash) => {
-                        if (err) throw err
-                        let passwordHash = hash
-                        collectionProfile
-                            .insertOne({
-                                    login: login,
-                                    password: passwordHash
-                                },
-                                function(err, profile){
-                                    callback(data)
-                                })
-                    })
-                })
-            }
-        })
-    } else {
-        let data = {
-            login: login,
-            password: password,
-            repassword: repassword,
-            errors: errors,
-            layout: "auth"
-        }
-        callback(data)
-    }
-}
+      regLogin: regLogin,
+      regPassword: regPassword,
+      repassword: repassword,
+      errors: errors,
+      layout: "auth",
+    };
+    callback(data);
+  }
+};
